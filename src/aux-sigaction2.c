ebd7a01bc4160eaa97f9b5b056a64ef8a6026106
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-30 16:42:38 -0300

    Comming back to process

--- src/aux-sigaction2.c
+++ src/aux-sigaction2.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 15:21:08 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/30 16:35:08 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/30 16:40:51 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,23 @@ void handler_sigtstp(int sig)
 {
 	// Ctrl + Z will call this
 	printf("Stop not allowed\n");
+}
 
+// Ctrl +Z Now will stop. Fg will come back the program
+void handler_sigcont(int sig)
+{
+	printf("Input number:");
+	fflush(stdout);
 }
 
 int	aux_sigaction2()
 {
-	// struct sigaction sa;
-	// sa.sa_handler = &handler_sigtstp;
-	// sa.sa_flags = SA_RESTART;
-	// sigaction(SIGTSTP, &sa, NULL);
+	struct sigaction sa;
+	sa.sa_handler = &handler_sigtstp;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGCONT, &sa, NULL);
 
-	signal(SIGTSTP, &handler_sigtstp);
+	// signal(SIGTSTP, &handler_sigtstp);
 
 
 	int x;

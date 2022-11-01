19e82ddb128783312b74ef3bb8bba0fd5aa57dcd
Rodrigo Lins <rlinsdev@gmail.com>
2022-11-01 07:42:46 -0300

    Mini mini - partial - read line

--- src/mini-mini-shell.c
+++ src/mini-mini-shell.c
@@ -6,7 +6,7 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 17:29:43 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/31 21:11:37 by rlins            ###   ########.fr       */
+/*   Updated: 2022/11/01 07:41:48 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,13 @@ static void	lsh_loop();
 static char *lsh_read_line();
 static char **lsh_split_line(char *line);
 static int lsh_execute(char **args);
+static void check_buffer_error(char *buffer);
 
 
 void mini_mini_shell()
 {
 	printf("yep!");
 	lsh_loop();
-}
-
-char *lsh_read_line()
-{
-	int bufsize = BUFFER_SIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(char) * bufsize);
-	int c;
-
-	if (!buffer)
-	{
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
 }
 
 static void	lsh_loop()
@@ -55,4 +42,46 @@ static void	lsh_loop()
 		free(args);
 
 	} while (status);
+}
+
+static void check_buffer_error(char *buffer)
+{
+	if (!buffer)
+	{
+		fprintf(stderr, "Allocation error \n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static char *lsh_read_line()
+{
+	int bufsize = BUFFER_SIZE;
+	int position  = 0;
+	int c;
+	char *buffer = malloc(sizeof(char) * bufsize);
+
+	check_buffer_error(buffer);
+
+	while (1)
+	{
+		c = getchar();
+
+		if (c == EOF || c == '\n')
+		{
+			buffer[position] = '\0';
+			return buffer;
+		}
+		else
+		{
+			buffer[position] = c;
+		}
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += BUFFER_SIZE;
+			buffer = realloc(buffer, bufsize);
+			check_buffer_error(buffer);
+		}
+	}
 }

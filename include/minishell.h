ae5e3b713128c0d1941d1991ce695987a65f7507
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-31 21:13:19 -0300

    Mini mini - partial

--- include/minishell.h
+++ include/minishell.h
@@ -6,12 +6,13 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 09:54:12 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/31 17:34:18 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/31 21:06:31 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# define BUFFER_SIZE 1024;
 
 # include <libft.h>
 # include <stdio.h> // Printf
--- src/mini-mini-shell.c
+++ src/mini-mini-shell.c
@@ -6,15 +6,53 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 17:29:43 by rlins             #+#    #+#             */
-/*   Updated: 2022/10/31 17:35:30 by rlins            ###   ########.fr       */
+/*   Updated: 2022/10/31 21:11:37 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <minishell.h>
 
+static void	lsh_loop();
+static char *lsh_read_line();
+static char **lsh_split_line(char *line);
+static int lsh_execute(char **args);
+
 
 void mini_mini_shell()
 {
-	printf("entrou");
+	printf("yep!");
+	lsh_loop();
+}
 
+char *lsh_read_line()
+{
+	int bufsize = BUFFER_SIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c;
+
+	if (!buffer)
+	{
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void	lsh_loop()
+{
+	char *line;
+	char **args;
+	int  status;
+
+	do {
+		printf("> ");
+
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+
+	} while (status);
 }
